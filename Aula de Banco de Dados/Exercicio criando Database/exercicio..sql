5fd67e8d46837f7796b43c6c8cdd43f4f88a3789
indianaraguiapd <157187549+indianaraguiapd@users.noreply.github.com>
2025-07-17 18:32:38 -0300

    Add files via upload

---
+++ Aula de Banco de Dados/Exercicio criando Database/exercicio..sql
@@ -0,0 +1,8 @@
+ CREATE TABLE alunosss (
+    ->     nome varchar(255) NOT NULL,
+    ->     matricula varchar(255) NOT NULL,
+    ->     data_nascimento date,
+    ->     PRIMARY KEY (matricula)
+    -> );
+    INSERT INTO Alunos values('Indianara', 'pdita382', '2000-05-06');
+    INSERT INTO Alunos values('Indiana', 'pdita444', '1995-01-24');
